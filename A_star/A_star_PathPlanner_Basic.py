d82ac9ff99acb6a94a56f8d8fd94e44809f0137f
EunGiHan <69252153+EunGiHan@users.noreply.github.com>
2021-08-01 00:29:10 +0900

    update basic path planner

--- A_star/A_star_PathPlanner_Basic.py
+++ A_star/A_star_PathPlanner_Basic.py
@@ -13,28 +13,30 @@ class Obstacle:
 
 
 class PathPlanner:
-    def __init__(self, init_pos, init_heading, predict_step, predict_range, move_unit, fin_pos):
-        self.edge_points = np.array([[0, 0], [50, 0], [50, 50], [0, 50]])
+    def __init__(self):
+        self.edge_points = np.array([[0, 0], [30, 0], [30, 30], [0, 30]])
         self.obstacle = Obstacle()
         self.ob_list = self.obstacle.ob_list
 
-        self.init_pos = np.array(init_pos)
-        self.cur_pos = np.array(init_pos) #[x, y] 현재 배가 있는 위치
-        self.cur_heading = np.array(init_heading) #[x, y] 벡터!, normalize 수시로 필요!
-        self.trajectory = np.zeros((1,2))  # [x, y]
-        self.fin_pos = fin_pos
+        self.init_pos = np.array([1, 1])
+        self.cur_pos = self.init_pos  # [x, y] 현재 배가 있는 위치
+        self.cur_heading = np.array([1, 0])  # [x, y] 벡터!, normalize 수시로 필요!
+        self.trajectory = np.zeros((1, 2))  # [x, y]
+        self.end_pos = [25, 25]
 
-        self.predict_step = predict_step
-        self.predict_range = predict_range
-        self.obstacle_search_range = 1 #이거 파라미터로 전달받기
+        self.predict_step = 5
+        self.predict_step_size = 1.5
+        self.obstacle_search_range = 1
 
-        self.move_unit = move_unit
-        self.goal_range = 1 #이거 파라미터로 전달받기
+        self.move_size = 1
+        self.past_path = np.array([self.init_pos])
+        self.goal_range = 1
 
         self.make_trajectory()
 
     def is_finished(self):
-        if math.sqrt((self.cur_pos[0]-self.fin_pos[0])**2 + (self.cur_pos[1]-self.fin_pos[1])**2) <= self.goal_range:
+        if math.sqrt(
+                (self.cur_pos[0] - self.end_pos[0]) ** 2 + (self.cur_pos[1] - self.end_pos[1]) ** 2) <= self.goal_range:
             return True
         else:
             return False
@@ -45,46 +47,54 @@ class PathPlanner:
 
         search_center, predict_heading = self.best_point(search_center, predict_heading)
 
-        self.trajectory = search_center
+        self.trajectory = search_center  # 초기화
 
         for i in range(self.predict_step - 1):
-            search_center, predict_heading = self.best_point(search_center, predict_heading)  # 각 값이 잘 들어가 있는지 확인!
+            search_center, predict_heading = self.best_point(search_center, predict_heading)
             self.trajectory = np.append(self.trajectory, search_center, axis=0)
 
     def best_point(self, search_center, predict_heading):
-        min_f_value = 1000000
+        min_f_value = float("Inf")
         best_point = np.zeros((0, 2))
         best_heading = np.zeros((0, 2))
-        points = self.predict_range * np.array(
+        points = self.predict_step_size * np.array(
             [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]])
-        for i in range(8): #더 좋은 방법 있나 찾기
+        for i in range(8):  # 더 좋은 방법 있나 찾기
             points[i][0] += search_center[0][0]
             points[i][1] += search_center[0][1]
 
         for p in points:
             if self.is_line_out(p):
-                continue  # 다른 포인트 찾아 for문으로 돌아가는지 아니면 while 문으로 돌아가는지 확인!
+                continue
             if self.is_obstacle_in(p):
                 continue
-            ##8개 다 돌았는데 갈 수 있는 곳 없는 경우 해결!!
 
             # g값 중 시작부터 거리를 계산함
             distance_to_cur = (p[0] - self.cur_pos[0]) ** 2 + (p[1] - self.cur_pos[1]) ** 2
 
-            # g값 중 회전 가중치를 계산함 -- 마이너스로 잘 들어가는지, 180도 이상인 둔각에서는 잘 되는지 필시 확인!
+            # g값 중 회전 가중치를 계산함
             vec = p - search_center[0]
-            vec = vec / math.sqrt((vec[0] ** 2 + vec[1] ** 2)) # vec = vec / np.linalg.norm(vec)
+            vec = vec / math.sqrt((vec[0] ** 2 + vec[1] ** 2))
             sin_theta = round(predict_heading[0] * vec[1] - predict_heading[1] * vec[0], 5)
             theta = math.asin(sin_theta) * 180 / math.pi  # degree 값
 
+            if 45 >= abs(theta):
+                rotate_cost_gain = 1
+            # elif 60 >= abs(theta):
+            #     rotate_cost_gain = 1.2
+            elif 90 >= abs(theta):
+                rotate_cost_gain = 1.2
+            else:
+                rotate_cost_gain = 2
+
             distance_to_search = (p[0] - search_center[0][0]) ** 2 + (p[1] - search_center[0][1]) ** 2
-            rotate_cost = distance_to_search * abs(theta * 1.1)
+            rotate_cost = distance_to_search * rotate_cost_gain
 
             # g값 합침
             g = distance_to_cur + rotate_cost
 
             # h값을 계산함
-            h = (p[0] - self.fin_pos[0]) ** 2 + (p[1] - self.fin_pos[1]) ** 2
+            h = (p[0] - self.end_pos[0]) ** 2 + (p[1] - self.end_pos[1]) ** 2
 
             # f값 총합
             f = g + h
@@ -122,10 +132,12 @@ class PathPlanner:
         print(self.cur_pos)
 
         vec = self.trajectory[0] - self.cur_pos
-        vec = vec / math.sqrt((vec[0]**2 + vec[1]**2))
-        next = vec * self.move_unit
+        vec = vec / math.sqrt((vec[0] ** 2 + vec[1] ** 2))
+        next = vec * self.move_size
+
         self.cur_pos = self.cur_pos + next
         self.cur_heading = vec
+        self.past_path = np.append(self.past_path, np.array([self.cur_pos]), axis=0)
 
     def obstacle_update(self):
         self.ob_list = self.obstacle.ob_list
@@ -134,7 +146,7 @@ class PathPlanner:
 def main():
     ## GPS 좌표 처리하는 기능 추가해야 함!
     print("----------경로 추종 클래스 할당------------")
-    path_planner = PathPlanner([2, 2], [1, 0], 5, 1.5, 1, [10, 10])
+    path_planner = PathPlanner()
     print("---------경로 추종 클래스 할당 끝----------")
 
     while not path_planner.is_finished():
