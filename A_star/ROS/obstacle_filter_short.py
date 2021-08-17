58aca37441513db1a36dadad619bc3eaf9aea338
EunGiHan <69252153+EunGiHan@users.noreply.github.com>
2021-08-17 14:57:15 +0900

    change ob_list pub axis, etc

--- A_star/ROS/obstacle_filter_short.py
+++ A_star/ROS/obstacle_filter_short.py
@@ -16,14 +16,14 @@ class ObstacleFilter:
         self.min_wall_length = ob_config['min_wall_length']
 
         ## lists
-        self.ob_list = np.zeros((1, 3)) #(x from lidar, y from lidar, theta from lidar)
+        self.ob_list = np.zeros((1, 3)) #(x, y, theta)
 
         ## ROS
         rospy.Subscriber('/obstacles', Obstacles, self.obstacle_callback)
         self.filtered_obstacles_pub = rospy.Publisher("/filtered_obstacles", FilteredObstacles, queue_size=10)
 
     def obstacle_callback(self, msg):
-        self.ob_list = np.zeros((1, 3)) 
+        self.ob_list = np.zeros((1, 3)) #init
 
         ## circle filtering
         circle_list = msg.circles #.center.x/.center.y/.center.z/.radius
@@ -31,8 +31,7 @@ class ObstacleFilter:
             x = -circle_list[i].center.x #deleted (-) !!! check!!!!!
             y = circle_list[i].center.y
             d = math.sqrt(x ** 2 + y ** 2)
-            theta = math.atan2(y,x) * math.pi / 180
-
+            theta = self.convert_theta(math.degrees(math.atan2(y,x)))
             if d < self.obstacle_search_range:
                 self.ob_list = np.append(self.ob_list, np.array([[x, y, theta]]), axis=0)
             else:
@@ -45,30 +44,40 @@ class ObstacleFilter:
             start_y = segment_list[i].first_point.y
             end_x = segment_list[i].last_point.x
             end_y = segment_list[i].last_point.y
-            distance = self.distance_boat_to_wall(start_x, start_y, end_x, end_y)
+            d = self.distance_boat_to_wall(start_x, start_y, end_x, end_y)
             length = math.sqrt((end_x - start_x) ** 2 + (end_y - start_y) ** 2)
 
-            if (distance < self.obstacle_search_range) and (abs(length) > self.min_wall_length):
-                div_num = int(length/2 - 1)
+            if (d < self.obstacle_search_range) and (abs(length) > self.min_wall_length):
+                div_num = int(length/2 - 1) # divide wall by 0.5(m)
                 for i in range(div_num):
                     mid_x = start_x + (end_x-start_x)*(i/div_num)
                     mid_y = start_y + (end_y-start_y)*(i/div_num)
-                    w_theta = math.atan2(mid_y,mid_x) * math.pi / 180
+                    w_theta = self.convert_theta(math.degrees(math.atan2(mid_y,mid_x)))
                     self.ob_list = np.append(self.ob_list, np.array([[mid_x, mid_y, w_theta]]), axis = 0)
-                self.ob_list = np.append(self.ob_list, np.array([[end_x, end_y, (math.atan2(end_y, end_x) * math.pi / 180)]]), axis = 0)
+                end_theta = self.convert_theta(math.degrees(math.atan2(end_y,end_x)))
+                self.ob_list = np.append(self.ob_list, np.array([[end_x, end_y, end_theta]]), axis = 0)
             else:
                 continue
         
         self.ob_list = np.delete(self.ob_list, 0, axis=0)
 
     def distance_boat_to_wall(self, start_x, start_y, end_x, end_y):
-        boat_to_start = start_x**2 + start_y**2 #square value
-        boat_to_end = end_x**2 + end_y**2 # square value
-        start_to_end = (start_x-end_x)**2 + (start_y-end_y)**2 #square value
-        cosin_value = (boat_to_start + start_to_end - boat_to_end)/(2*math.sqrt(boat_to_start)*math.sqrt(start_to_end))
-        angle_value = math.acos(cosin_value)
-        boat_to_line = abs(math.sqrt(boat_to_start)*math.sin(angle_value))
-        return boat_to_line
+        area = abs(start_x*end_y - start_y*end_x) * 0.5
+        start_to_end = math.sqrt((start_x-end_x)**2 + (start_y-end_y)**2)
+        return area/start_to_end
+
+    def convert_theta(self, angle):
+        if 0 <= angle <= 90:
+            theta = angle
+        elif 90 < angle <= 180:
+            theta = -angle + 90
+        elif -90 <= angle < 0:
+            theta = -angle + 90
+        else:
+            theta = -angle - 270
+
+        return theta
+
 
     def filtered_obstacles_publisher(self):
         filtered_obstacles = FilteredObstacles()
